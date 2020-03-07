61c0a1ea2f90a7bb98e80f9cff3cf5eea554140a
김찬기 <chanki@gimchangiui-MacBook-Pro.local>
2020-03-07 22:45:46 +0900

    Updates

---
+++ precache-manifest.c5f0a312bf6196dac9ca166cd1ba0f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fcbfa8ecdcd4231c8f4844599602bb0",
+    "url": "/react-news/index.html"
+  },
+  {
+    "revision": "bb6ac50e94cd6cc1be68",
+    "url": "/react-news/static/css/main.791ccc96.chunk.css"
+  },
+  {
+    "revision": "91c702033deea5137a43",
+    "url": "/react-news/static/js/2.18b21da0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-news/static/js/2.18b21da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6ac50e94cd6cc1be68",
+    "url": "/react-news/static/js/main.ed528343.chunk.js"
+  },
+  {
+    "revision": "6573f40a5068fbac04dc",
+    "url": "/react-news/static/js/runtime-main.e19033fd.js"
+  }
+]);
